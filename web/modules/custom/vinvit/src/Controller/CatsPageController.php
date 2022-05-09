b2f001f91ef9bcf444e609ef120a11fa3671e349
vinvit <vitalik00pr@gmail.com>
2022-05-09 11:42:10 +0300

    add output data from database

--- web/modules/custom/vinvit/src/Controller/CatsPageController.php
+++ web/modules/custom/vinvit/src/Controller/CatsPageController.php
@@ -17,9 +17,16 @@ class CatsPageController extends ControllerBase {
    */
   public function content(): array {
     $add_form = \Drupal::formBuilder()->getForm('Drupal\vinvit\Form\CatsForm');
+
+    $conn = \Drupal::database()->select('vinvit', 'v');
+    $conn->fields('v', ['id', 'cat_name', 'email', 'cat_image', 'date']);
+    $conn->orderBy('date', 'DESC');
+    $results = $conn->execute()->fetchAll();
+
     return [
       '#theme' => 'cats',
       '#add_form' => $add_form,
+      '#cats' => $results,
       '#markup' => 'Hello! You can add here a photo of your cat.',
     ];
   }
